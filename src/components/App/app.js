8c71a114be935bcf46e7ff83836ed27885cf2996
ShUmarbek <shumarbek001@gmail.com>
2026-06-06 16:16:31 +0500

    Third commit

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import AppInfo from '../App-info/app-info'
 import ''
+import SearchPanel from '../Search-panel/search-panel'
+import AppFilter from '../App-filter/app-filter'
 
 const app = () => {
   return (
     <div className='app font-monospace'>
       <div className='content'>
         <AppInfo/>
-        <div className='search'></div>
+        <div className='search-panel'>
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
       </div>
     </div>
   )
---
+++ src/components/Search-panel/search-panel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './search-panel.css'
+
+function SearchPanel() {
+  return (
+    <input type="text" className='form-control search-input' placeholder='Kinolarni qidirish...' />
+  )
+}
+
+export default SearchPanel
