61b87fe7667b99b80cfe84fcddc245dddb5967e7
ShUmarbek <shumarbek001@gmail.com>
2026-06-06 16:03:40 +0500

    Second commit

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import AppInfo from '../App-info/app-info'
+import ''
 
 const app = () => {
   return (
-    <div>
-      
+    <div className='app font-monospace'>
+      <div className='content'>
+        <AppInfo/>
+        <div className='search'></div>
+      </div>
     </div>
   )
 }
